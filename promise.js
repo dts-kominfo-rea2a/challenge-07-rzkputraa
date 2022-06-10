6c422426432670ccea20dd950ec498770aa19ba2
Rizki Putra Wicaksono <rizkiputra1100@gmail.com>
2022-06-10 15:48:56 +0700

    fix tugas promise

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (sum) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  const theater = theaterIXX.concat(theaterVGC);
+
+  return theater.filter((item) => item.hasil === sum).length;
+};
 
 module.exports = {
   promiseOutput,
